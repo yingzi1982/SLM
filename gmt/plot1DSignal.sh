5ca2a3a5478cd010dfc61f5650d6ce5364bbf4d0
yingzi1982 <yingzi.ying@me.com>
2023-11-27 18:50:52 +0100

    pushing to Github

--- gmt/plot1DSignal.sh
+++ gmt/plot1DSignal.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-#gmt defaults > gmt.conf
 rm -f gmt.conf
 rm -f gmt.history
 #gmt set MAP_FRAME_TYPE plain
@@ -9,18 +8,33 @@ gmt set FONT 12p,Helvetica,black
 
 name=${1}
 
-xrange=${2}
-xtick=${3}
-xlabel=${4}
+width=${2}
+height=${3}
+line=${4}
+fill=${5}
 
-yrange=${5}
-ytick=${6}
-ylabel=${7}
+xrange=${6}
+xInterval=${7}
+xlabel=${8}
 
-line=thinnest,black
-fill=lightgray
-width=2.2i
-height=0.78i
+yrange=${9}
+yInterval=${10}
+ylabel=${11}
+#echo $name 
+#echo $width
+#echo $height
+#echo $line
+#echo $fill
+#echo $xrange
+#echo aaaa
+#echo $xInterval
+#echo $xlabel
+#exit
+#
+#width=2.2i
+#height=0.78i
+#line=thinnest,black
+#fill=lightgray
 
 backupFolder=../backup/
 figFolder=../figures/
@@ -35,15 +49,15 @@ xmin=`echo $xrange | awk '{print $1}'`
 xmax=`echo $xrange | awk '{print $2}'`
 ymin=`echo $yrange | awk '{print $1}'`
 ymax=`echo $yrange | awk '{print $2}'`
-sub=2
-xtickSub=`echo "(($xtick)/$sub)" | bc -l`
-ytickSub=`echo "(($ytick)/$sub)" | bc -l`
+divisor=2
+xSubInterval=`echo "(($xInterval)/$divisor)" | bc -l`
+ySubInterval=`echo "(($yInterval)/$divisor)" | bc -l`
 
 region=$xmin/$xmax/$ymin/$ymax
 
 gmt begin $fig
 
-gmt plot $originalxy -J$projection -R$region -Bxa$xtick\f$xtickSub+l"$xlabel" -Bya$ytick\f$ytickSub+l"$ylabel" -G$fill -W$line
+gmt plot $originalxy -J$projection -R$region -Bxa$xInterval\f$xSubInterval+l"$xlabel" -Bya$yInterval\f$ySubInterval+l"$ylabel" -G$fill -W$line
 
 gmt end
 
