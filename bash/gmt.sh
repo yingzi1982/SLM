327227aa6dc8e40998a11515973e058183e54d3b
yingzi1982 <yingzi.ying@me.com>
2023-11-27 18:59:11 +0100

    pushing to Github

--- bash/gmt.sh
+++ bash/gmt.sh
@@ -3,5 +3,5 @@
 cd ../gmt/
 
 if true; then
-  ./plot1DSignal.sh soundPressureEnvelope 2.4i 0.8i thinnest,black lightgray "0 60" 20 "Time (s)" "-1 1" 1 "Signal (Pa)"
+  ./plot1DSignal.sh soundPressureEnvelope 2.2i 0.8i thinnest,black lightgray "0 60" 20 "Time (s)" "-1 1" 1 "Signal (Pa)"
 fi
