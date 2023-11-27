5937a43ac98cecd60546d3d45961fd64bba3f43b
yingzi1982 <yingzi.ying@me.com>
2023-11-27 22:33:42 +0100

    pushing to Github

--- bash/gmt.sh
+++ bash/gmt.sh
@@ -6,5 +6,5 @@ if true; then
   ./plot1DSignal.sh soundPressureEnvelope 2.2i 0.8i -Wthinnest,black -Glightgray "0 60" 20 "Time (s)" "-1 1" 1 "Signal (Pa)"
 fi
 if true; then
-  ./plot1DSignal.sh soundPressureLevel 2.2i 0.8i -Wthinnest,black " " "20 2000" 500 "Freq (Hz)" "-60 60" 20 "SPL (Pa/Hz)"
+  ./plot1DSignal.sh soundPressureLevel 2.2i 0.8i -Wthinnest,black " " "900 1100" 500 "Freq (Hz)" "-60 60" 20 "PSD (Pa/Hz)"
 fi
