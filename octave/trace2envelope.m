81e29326b694ad2ca804443ec2cdd9bdd9961b78
yingzi1982 <yingzi.ying@me.com>
2023-11-27 17:14:59 +0100

    pushing to Github

--- octave/trace2envelope.m
+++ octave/trace2envelope.m
@@ -5,9 +5,7 @@ t = signal(:,1);
 s = signal(:,2:end);
 
 envPositive = [t abs(hilbert(s))];
-%envPositive([1,end],:) = [];
 envNegative = [t -abs(hilbert(-s))];
-%envNegative([1,end],:) = [];
 env = [envPositive;flipud(envNegative)];
 env = resample(env,np,length(env));
 
