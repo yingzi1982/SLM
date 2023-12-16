212a1f80f468a953e0f4ffacb27849c914acc42c
yingzi1982 <yingzi.ying@me.com>
2023-12-16 18:28:47 +0100

    pushing to Github

--- gmt/plotBar.sh
+++ gmt/plotBar.sh
@@ -37,7 +37,7 @@ region=$xmin/$xmax/$ymin/$ymax
 
 gmt begin $fig
 
-awk '{print $1, $3}' $originalxy | gmt plot -J$projection -Bx+l"$xlabel" -By+l"$ylabel" -BWSne+glightblue -R$region -Sb1q+b0 -W0.25p 
+awk '{print $1, $3}' $originalxy | gmt plot -J$projection -Bx+l"$xlabel" -By+l"$ylabel" -BWSne+glightblue -R$region -Sb 
 
 gmt end
 
