a43c8782abf0396736e0024c1ef48fe46d90fa5f
yingzi1982 <yingzi.ying@me.com>
2023-12-16 21:23:07 +0100

    pushing to Github

--- bash/read_LA.sh
+++ bash/read_LA.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 wavFolder=$1
-echo $wavFolder
 
-LAeqFile=`ls $wavFolder\/*Spectrum_Report.txt`
-dos2unix -q $LAeqFile
+Report_file=`ls -1 $wavFolder\/*_Report.txt | head -n 1`
+Spectrum_Report_file=`ls -1 $wavFolder\/*_Report.txt | tail -n 1`
 
-thirdOctaveBand=`awk 'NR==18{for(i=8;i<=NF;++i)print $i}' $LAeqFile`
+dos2unix -q $Report_file
+dos2unix -q $Spectrum_Report_file
 
-LAeq=`awk 'NR==20{for(i=5;i<=NF;++i)if($i<0||$i=="-nan"){print 0}else{print $i}}' $LAeqFile`
+LAeq_fullband=`awk 'NR==21{print $14}' $Report_file`
+echo 0 fullband $LAeq_fullband >  ../backup/LA
 
-paste <(echo "$thirdOctaveBand") <(echo "$LAeq") --delimiters ' ' | cat -n > ../backup/LA
+thirdOctaveBand=`awk 'NR==18{for(i=8;i<=NF;++i)print $i}' $Spectrum_Report_file`
+
+LAeq=`awk 'NR==20{for(i=5;i<=NF;++i)if($i<0||$i=="-nan"){print 0}else{print $i}}' $Spectrum_Report_file`
+
+paste <(echo "$thirdOctaveBand") <(echo "$LAeq") --delimiters ' ' | cat -n >> ../backup/LA
