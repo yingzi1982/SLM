3958a98747224a78a404f6ed57f4ca31004b0c2e
yingzi1982 <yingzi.ying@me.com>
2023-12-18 09:47:07 +0100

    pushing to Github

--- gmt/plotBar.sh
+++ gmt/plotBar.sh
@@ -40,11 +40,12 @@ yHalfInterval=`echo $yInterval/2 | bc -l`
 LAF90=`awk 'NR==2{print $3}' $originalxy`
 LAFmax=`awk 'NR==3{print $3}' $originalxy`
 
+arrowStart_x=2
 cat << EOF >| LAF90.txt
-0 $LAF90 LM L@-AF90@-
+$arrowStart_x $LAF90 $xmin $LAF90 LM L@-AF90@-
 EOF
 cat << EOF >| LAFmax.txt
-0 $LAFmax LM L@-AFmax@-
+$arrowStart_x $LAFmax $xmin $LAFmax LM L@-AF90@-
 EOF
 
 gmt begin $fig
@@ -55,11 +56,14 @@ awk 'NR>=5{print $1, $3}' $originalxy | gmt plot -Sb1ub0 -Gorange -W.5p
 #echo 0 $LAFmax | gmt plot  -Ss0.02i -Gred -N -W0p
 #echo 0 $LAF90 | gmt plot  -Ss0.02i -Ggreen -N -W0p
 
-gmt plot -Ss0.03i -Gred -Wthinnest LAFmax.txt
-gmt text -Dj10p/0 -F+fred+j -N LAFmax.txt
+#gmt plot -Ss0.03i -Gred -Wthinnest LAFmax.txt
+awk '{print $1, $2, $3, $4 }' LAF90.txt | gmt plot -N -Sv0.4c+s+e -Gblack -W2p
+
+
+#gmt text -Dj10p/0 -F+fred+j -N LAFmax.txt
 
 gmt plot -Ss0.03i -Gblue -Wthinnest LAF90.txt
-gmt text -Dj10p/0 -F+fblue+j -N LAF90.txt
+#gmt text -Dj10p/0 -F+fblue+j -N LAF90.txt
 
 gmt end
 
